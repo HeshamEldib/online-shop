875e7b1ab4d71133046a9c02051a84db82bbf5e8
HeshamEldib <heshameldib27@gmail.com>
2023-10-21 21:45:19 +0300

    navbar => search

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,72 +6,110 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLocationDot,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
-function MainNavbar() {
+import "./navbar.css";
+
+export default function MainNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary main-navbar">
       <Container fluid>
-        <Link to="/" className="navbar-brand">
-          Hesham Eldib
-        </Link>
-        {/* <Navbar.Brand href="/">Hesham Eldib</Navbar.Brand> */}
-
-        {/* location */}
-        <Button>
-          <FontAwesomeIcon icon={faLocationDot} />
-        </Button>
+        <NavLeft />
 
         {/* search */}
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+        <Search />
 
         {/* menu */}
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
+        {/* <Navbar.Collapse id="navbarScroll">
+        </Navbar.Collapse> */}
 
-        <Navbar.Collapse id="navbarScroll">
-          {/* links */}
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <NavDropdown title="EN" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Hello" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link href="#action1">Returns</Nav.Link>
-            <Nav.Link href="#action2">Cart</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {/* links */}
+        <NavRight />
       </Container>
     </Navbar>
   );
 }
 
-export default MainNavbar;
+function NavLeft() {
+  return (
+    <div className="d-flex align-items-center nav-left">
+      <Link to="/" className="navbar-brand logo">
+        Hesham Eldib
+      </Link>
+
+      {/* location */}
+      <Location />
+    </div>
+  );
+}
+
+function Location() {
+  return (
+    <div className="but-country d-flex">
+      <FontAwesomeIcon icon={faLocationDot} />
+      <span className="d-inline-block">
+        <span className="w-100 d-inline-block">Deliver to</span>
+        <span className="country">Egypt</span>
+      </span>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <Form className="d-flex search">
+      <Form.Control
+        type="search"
+        placeholder="Search"
+        className="search-input"
+        aria-label="Search"
+      />
+      <Button variant="outline-success" className="but-search">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </Button>
+    </Form>
+  );
+}
+
+function NavRight() {
+  return (
+    <Nav
+      className="me-auto my-2 my-lg-0 nav-right"
+      style={{ maxHeight: "100px" }}
+      navbarScroll
+    >
+      <NavDropdown
+        title="EN"
+        id="navbarScrollingDropdown"
+        className="right-link"
+      >
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+
+      <NavDropdown
+        title="Hello"
+        id="navbarScrollingDropdown"
+        className="right-link"
+      >
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+
+      <Link to="/" className="nav-link right-link">
+        Returns
+      </Link>
+      <Link to="/" className="nav-link right-link">
+        Cart
+      </Link>
+    </Nav>
+  );
+}
